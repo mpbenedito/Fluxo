27b204ec8b26bc2384ccbc63a9361ff8ffff1d0a
Marcelo de Paula Benedito <mpbenedito@gmail.com>
2020-01-18 10:36:04 -0300

    Início do Projeto

---
+++ MapaMental/Fluxo de Caixa.mm
@@ -0,0 +1,43 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1579349775593" ID="ID_383988035" MODIFIED="1579353573849" TEXT="Fluxo de Caixa">
+<node CREATED="1579350190415" ID="ID_1769972442" MODIFIED="1579354256750" POSITION="right" TEXT="1 - Caracteristicas Gerais">
+<node CREATED="1579350004723" ID="ID_1851993246" MODIFIED="1579350022200" TEXT="Controle de 1 a N Planejamentos"/>
+<node CREATED="1579350058271" ID="ID_514384372" MODIFIED="1579350102944" TEXT="Controle de Visualiza&#xe7;&#xe3;o e Manuten&#xe7;&#xe3;o"/>
+<node CREATED="1579350110327" ID="ID_1294344859" MODIFIED="1579350121544" TEXT="Usu&#xe1;rio Administrador pode enxergar tudo"/>
+<node CREATED="1579350261137" ID="ID_360031746" MODIFIED="1579350267839" TEXT="Lan&#xe7;amentos com Grupo Subgrupo"/>
+<node CREATED="1579350291139" ID="ID_114809346" MODIFIED="1579350334287" TEXT="Registrar movimentos banc&#xe1;rios e Cart&#xe3;o de Cr&#xe9;dito"/>
+<node CREATED="1579350344991" ID="ID_1428204112" MODIFIED="1579350361215" TEXT="Registrar 1 a N bancos por Planejamento"/>
+<node CREATED="1579350365717" ID="ID_345268676" MODIFIED="1579350378623" TEXT="Registrar 1 a N cart&#xf5;es por Planejamento"/>
+<node CREATED="1579350626484" ID="ID_195881288" MODIFIED="1579350661975" TEXT="Registrar Previs&#xf5;es de Receitas/Despesas "/>
+<node CREATED="1579350712886" ID="ID_1472684300" MODIFIED="1579353510627" TEXT="Considerar Bancos e Cart&#xf5;es na compara&#xe7;&#xe3;o com Previs&#xf5;es">
+<node CREATED="1579353449251" ID="ID_176715311" MODIFIED="1579353486196" TEXT="Receitas x Despesas"/>
+<node CREATED="1579353491242" ID="ID_397456162" MODIFIED="1579353501616" TEXT="Filtros de Anos, meses"/>
+<node CREATED="1579353510628" ID="ID_749913341" MODIFIED="1579353517192" TEXT="Compara&#xe7;&#xf5;es de Meses"/>
+<node CREATED="1579353530806" ID="ID_469641020" MODIFIED="1579353539744" TEXT="Compara&#xe7;&#xf5;es de Per&#xed;odos"/>
+<node CREATED="1579353606924" ID="ID_877505234" MODIFIED="1579353615231" TEXT="Gr&#xe1;ficos"/>
+</node>
+</node>
+<node CREATED="1579349833622" ID="ID_551581491" MODIFIED="1579354263342" POSITION="right" TEXT="2 - Planejamentos">
+<node CREATED="1579353657317" ID="ID_438933826" MODIFIED="1579353670095" TEXT="Ter um respons&#xe1;vel"/>
+<node CREATED="1579353672813" ID="ID_298761612" MODIFIED="1579353703328" TEXT="Controle de acesso de quem movimenta"/>
+<node CREATED="1579353719696" ID="ID_1650527897" MODIFIED="1579353843744" TEXT="Controle de acesso de quem o visualiza"/>
+<node CREATED="1579353777363" ID="ID_1081766880" MODIFIED="1579353871839" TEXT="Bloqueio para descontinu&#xe1;-lo"/>
+<node CREATED="1579353907621" ID="ID_1789975273" MODIFIED="1579354045383" TEXT="Tabela ZA3 - Planejamentos">
+<node CREATED="1579354063822" ID="ID_1659479191" MODIFIED="1579354092791" TEXT="Descri&#xe7;&#xe3;o: Tabela principal com a rela&#xe7;&#xe3;o de planejamentos cadastrados"/>
+<node CREATED="1579353927233" ID="ID_82004510" MODIFIED="1579353944054" TEXT="Dicion&#xe1;rio"/>
+<node CREATED="1579353949449" ID="ID_1155110483" MODIFIED="1579353965175" TEXT="&#xcd;ndice(s)"/>
+</node>
+<node CREATED="1579353994885" ID="ID_1362825244" MODIFIED="1579354057679" TEXT="Tabela ZA4 - Usu&#xe1;rios por Planejamento">
+<node CREATED="1579354063822" ID="ID_484263148" MODIFIED="1579354133663" TEXT="Descri&#xe7;&#xe3;o: Tabela de relacionamento de Usu&#xe1;rios e Planejamentos e seus respectivos direitos"/>
+<node CREATED="1579354012346" ID="ID_1923704179" MODIFIED="1579354022887" TEXT="Dicion&#xe1;rio"/>
+<node CREATED="1579354015709" ID="ID_1334347295" MODIFIED="1579354028895" TEXT="&#xcd;ndice(s)"/>
+</node>
+</node>
+<node CREATED="1579354197662" ID="ID_745552403" MODIFIED="1579354410660" POSITION="left" TEXT="3 - Grupos e SubGrupos">
+<node CREATED="1579354283772" ID="ID_1678337716" MODIFIED="1579354314495" TEXT="Espec&#xed;ficos para cada Planejamento"/>
+<node CREATED="1579354339768" ID="ID_1762094991" MODIFIED="1579354399814" TEXT="Gen&#xe9;ricos para serem utilizados tanto:em contas banc&#xe1;rias ou cart&#xf5;es"/>
+<node CREATED="1579354410660" ID="ID_272746261" MODIFIED="1579354478989" TEXT="Mesma base de lan&#xe7;amento das previs&#xf5;es, permitindo compara&#xe7;&#xf5;es"/>
+</node>
+</node>
+</map>
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Fluxo
-Planejamento de Orçamento (Fluxo de Caixa) desenvolvido em ADVPL / MVC
+# Planejamento Orçamentário Pessoal (Fluxo de Caixa) desenvolvido em MVC para ADVPL e PowerBI
+
+## Necessidade do Projeto 
+Eu e minha esposa controlamos nosso orçamento pessoal num Excel. Funciona direitinho lá, inclusive montei um Dashboard nele bem interessante onde acompanhamos facilmente o andamento das nossas receitas e despesas comparadas ás previsões lançadas. Inclusive ela não será abandonada, por enquanto, pois será minha base de comparação para ver se tudo deste projeto está funcionando bem. Se alguém se interessar, entra em contato que passo uma cópia e explico como funciona.
+Contudo tenho aquele comixão. "Estou estudando MVC. Estou estudando PowerBI. Porque não desenvolver o Fluxo de Caixa com ADVPL num banco SQL? Poderei aplicar os estudos **MVC do ADVPL** e do **PowerBI** numa aplicação só. Bom jeito de eu aplicar o que estou estudando e ver se tudo se integra direitinho.". E antes que alguém fale *Ainnnnn o PowerBi serve para mandar foguetes para a Lua e você quer ele controlando sua conta corrente? Que pobre!"*. Seguinte: a aplicação é minha; o tempo é meu; a dedicação de estudo também é minha; quem comprou cursos, vejam só, fui eu; quem está programando, "ô loco bixo", sou eu. Então, se não está feliz, vai procurar projeto em outro lugar. Bjs e não me liga :-*
+
+## Publicação e Prazo
+O projeto será executado aos poucos, conforme meu tempo livre. Não prometo atualizações diárias, entretanto coloquei um prazo pessoal para execução das programações no ADVPL até o fim de fevereiro/2020. À medida que eu for desenvolvendo publicarei e comentarei as alterações. 
+A etapa da integração com PowerBI eu acredito até fim de Março/2020.
+
+## Mapa Mental
+Está sendo desenvolvido em FreeMind ([Google](https://sourceforge.net/projects/freemind/)), um software de licensiamento livre, que pode ser baixado e utilizado sem grandes problemas. É bastante intuitivo e atende bem ás necessidades desta atividade.
+Uma cópia do mapa mental deste projeto está sendo disponibilizada na pasta */MapaMental*. Não está totalmente pronto e sofrerá alterações conforme ideias nova surjam e os desenvolvimentos necessitem de adaptação.
