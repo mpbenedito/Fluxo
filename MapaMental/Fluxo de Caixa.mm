25df56ce06df33771986c6df68832838a487a6cd
Marcelo de Paula Benedito <mpbenedito@gmail.com>
2020-01-20 06:38:58 -0300

    Mapa Mental Pronto

--- MapaMental/Fluxo de Caixa.mm
+++ MapaMental/Fluxo de Caixa.mm
@@ -1,7 +1,7 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1579349775593" ID="ID_383988035" MODIFIED="1579353573849" TEXT="Fluxo de Caixa">
-<node CREATED="1579350190415" ID="ID_1769972442" MODIFIED="1579354256750" POSITION="right" TEXT="1 - Caracteristicas Gerais">
+<node COLOR="#000000" CREATED="1579349775593" ID="ID_383988035" MODIFIED="1579512940505" TEXT="Fluxo de Caixa">
+<node CREATED="1579350190415" HGAP="87" ID="ID_1769972442" MODIFIED="1579357950128" POSITION="right" TEXT="1 - Caracteristicas Gerais" VSHIFT="40">
 <node CREATED="1579350004723" ID="ID_1851993246" MODIFIED="1579350022200" TEXT="Controle de 1 a N Planejamentos"/>
 <node CREATED="1579350058271" ID="ID_514384372" MODIFIED="1579350102944" TEXT="Controle de Visualiza&#xe7;&#xe3;o e Manuten&#xe7;&#xe3;o"/>
 <node CREATED="1579350110327" ID="ID_1294344859" MODIFIED="1579350121544" TEXT="Usu&#xe1;rio Administrador pode enxergar tudo"/>
@@ -18,26 +18,83 @@
 <node CREATED="1579353606924" ID="ID_877505234" MODIFIED="1579353615231" TEXT="Gr&#xe1;ficos"/>
 </node>
 </node>
-<node CREATED="1579349833622" ID="ID_551581491" MODIFIED="1579354263342" POSITION="right" TEXT="2 - Planejamentos">
+<node CREATED="1579349833622" HGAP="100" ID="ID_551581491" MODIFIED="1579389842951" POSITION="right" TEXT="2 - Planejamentos" VSHIFT="-16">
 <node CREATED="1579353657317" ID="ID_438933826" MODIFIED="1579353670095" TEXT="Ter um respons&#xe1;vel"/>
 <node CREATED="1579353672813" ID="ID_298761612" MODIFIED="1579353703328" TEXT="Controle de acesso de quem movimenta"/>
 <node CREATED="1579353719696" ID="ID_1650527897" MODIFIED="1579353843744" TEXT="Controle de acesso de quem o visualiza"/>
 <node CREATED="1579353777363" ID="ID_1081766880" MODIFIED="1579353871839" TEXT="Bloqueio para descontinu&#xe1;-lo"/>
-<node CREATED="1579353907621" ID="ID_1789975273" MODIFIED="1579354045383" TEXT="Tabela ZA3 - Planejamentos">
+<node CREATED="1579353907621" ID="ID_1789975273" MODIFIED="1579389711588" TEXT="Tabela ZA3 - Planejamentos">
 <node CREATED="1579354063822" ID="ID_1659479191" MODIFIED="1579354092791" TEXT="Descri&#xe7;&#xe3;o: Tabela principal com a rela&#xe7;&#xe3;o de planejamentos cadastrados"/>
-<node CREATED="1579353927233" ID="ID_82004510" MODIFIED="1579353944054" TEXT="Dicion&#xe1;rio"/>
-<node CREATED="1579353949449" ID="ID_1155110483" MODIFIED="1579353965175" TEXT="&#xcd;ndice(s)"/>
 </node>
-<node CREATED="1579353994885" ID="ID_1362825244" MODIFIED="1579354057679" TEXT="Tabela ZA4 - Usu&#xe1;rios por Planejamento">
+</node>
+<node CREATED="1579356464479" HGAP="104" ID="ID_543984278" MODIFIED="1579389838765" POSITION="right" TEXT="3 - Usu&#xe1;rios" VSHIFT="-31">
+<node CREATED="1579356525965" ID="ID_1560649788" MODIFIED="1579356594586" TEXT="Controle de Acesso a Manuten&#xe7;&#xe3;o de Tabelas"/>
+<node CREATED="1579356554829" ID="ID_406900703" MODIFIED="1579356588129" TEXT="Controle de Acesso a Movimentos"/>
+<node CREATED="1579353994885" ID="ID_1362825244" MODIFIED="1579356637752" TEXT="Tabela ZA4 - Usu&#xe1;rios por Planejamento">
 <node CREATED="1579354063822" ID="ID_484263148" MODIFIED="1579354133663" TEXT="Descri&#xe7;&#xe3;o: Tabela de relacionamento de Usu&#xe1;rios e Planejamentos e seus respectivos direitos"/>
-<node CREATED="1579354012346" ID="ID_1923704179" MODIFIED="1579354022887" TEXT="Dicion&#xe1;rio"/>
-<node CREATED="1579354015709" ID="ID_1334347295" MODIFIED="1579354028895" TEXT="&#xcd;ndice(s)"/>
 </node>
 </node>
-<node CREATED="1579354197662" ID="ID_745552403" MODIFIED="1579354410660" POSITION="left" TEXT="3 - Grupos e SubGrupos">
+<node CREATED="1579356933908" HGAP="38" ID="ID_10334588" MODIFIED="1579357587537" POSITION="left" TEXT="7 - Contas Banc&#xe1;rias" VSHIFT="-15">
+<node CREATED="1579389913144" ID="ID_766988406" MODIFIED="1579389927256" TEXT="Cadastro de Contas Banc&#xe1;rias por Planejamento"/>
+<node CREATED="1579389930068" ID="ID_972915922" MODIFIED="1579389939040" TEXT="Cada Planejamento pode ter mais de uma conta para ser controlada"/>
+<node CREATED="1579389946747" ID="ID_828263816" MODIFIED="1579389965840" TEXT="Controlar data de cria&#xe7;&#xe3;o e saldo banc&#xe1;rio na data"/>
+<node CREATED="1579389970523" ID="ID_197847401" MODIFIED="1579389981767" TEXT="Apenas usu&#xe1;rios autorizados podem cadastrar contas"/>
+<node CREATED="1579390094219" ID="ID_1430662603" MODIFIED="1579390103279" TEXT="Tabela ZA6 ">
+<node CREATED="1579390107579" ID="ID_182985516" MODIFIED="1579390140928" TEXT="Descri&#xe7;&#xe3;o: Cadastro de contas Banc&#xe1;rias lan&#xe7;adas por Planejamento"/>
+</node>
+</node>
+<node CREATED="1579354197662" HGAP="113" ID="ID_745552403" MODIFIED="1579389834580" POSITION="right" TEXT="4 - Grupos" VSHIFT="-28">
 <node CREATED="1579354283772" ID="ID_1678337716" MODIFIED="1579354314495" TEXT="Espec&#xed;ficos para cada Planejamento"/>
 <node CREATED="1579354339768" ID="ID_1762094991" MODIFIED="1579354399814" TEXT="Gen&#xe9;ricos para serem utilizados tanto:em contas banc&#xe1;rias ou cart&#xf5;es"/>
 <node CREATED="1579354410660" ID="ID_272746261" MODIFIED="1579354478989" TEXT="Mesma base de lan&#xe7;amento das previs&#xf5;es, permitindo compara&#xe7;&#xf5;es"/>
+<node CREATED="1579355248270" ID="ID_136292871" MODIFIED="1579355258860" TEXT="Tabela ZA1">
+<node CREATED="1579356224135" ID="ID_757349732" MODIFIED="1579389761983" TEXT="Descri&#xe7;&#xe3;o: Grupos Gerais de despesas e receitas"/>
+</node>
+</node>
+<node CREATED="1579357031674" ID="ID_1413215045" MODIFIED="1579357597569" POSITION="left" TEXT="8 - Movimentos Banc&#xe1;rios" VSHIFT="16">
+<node CREATED="1579390165915" ID="ID_1105297784" MODIFIED="1579390179831" TEXT="Permitir lan&#xe7;amentos passados e futuros"/>
+<node CREATED="1579390185861" ID="ID_653725947" MODIFIED="1579390201327" TEXT="Limite retroativo &#xe9; a data do saldo da conta"/>
+<node CREATED="1579390213964" ID="ID_1585538343" MODIFIED="1579390241191" TEXT="Sinal do movimento dado pelo grupo e subgrupo de lan&#xe7;amento"/>
+<node CREATED="1579390246322" ID="ID_1587771202" MODIFIED="1579390256311" TEXT="Permitir lan&#xe7;amentos de estorno, invertendo o sinal"/>
+<node CREATED="1579390265035" ID="ID_535335034" MODIFIED="1579390273535" TEXT="Permitir lan&#xe7;amentos futuros"/>
+<node CREATED="1579390277252" ID="ID_1783078288" MODIFIED="1579390285719" TEXT="Permitir lan&#xe7;amento de parcelamentos"/>
+<node CREATED="1579512571208" ID="ID_1497962758" MODIFIED="1579512591131" TEXT="Apenas usu&#xe1;rios com acesso podem realizar movimentos"/>
+</node>
+<node CREATED="1579356322616" HGAP="119" ID="ID_1078287506" MODIFIED="1579389830143" POSITION="right" TEXT="5 - SubGrupos" VSHIFT="-22">
+<node CREATED="1579356334636" ID="ID_1659345386" MODIFIED="1579356346586" TEXT="Espec&#xed;ficos para cada Grupo"/>
+<node CREATED="1579356354702" ID="ID_59560502" MODIFIED="1579356363818" TEXT="Podem ser D&#xe9;bito ou Cr&#xe9;dito"/>
+<node CREATED="1579356378866" ID="ID_1069918231" MODIFIED="1579356388106" TEXT="Deve servir para Previs&#xf5;es e Movimentos"/>
+<node CREATED="1579389799326" ID="ID_426225907" MODIFIED="1579389811608" TEXT="Diferenciar Movimento de pagamento de cart&#xe3;o"/>
+<node CREATED="1579355263190" ID="ID_1184511308" MODIFIED="1579355270485" TEXT="Tabela ZA2">
+<node CREATED="1579356289485" ID="ID_1167266539" MODIFIED="1579389787024" TEXT="Descri&#xe7;&#xe3;o: Detalhamento dos Grupos e podem ser d&#xe9;bitos ou cr&#xe9;ditos"/>
+</node>
+</node>
+<node CREATED="1579357495376" HGAP="75" ID="ID_214400809" MODIFIED="1579512631689" POSITION="left" TEXT="9 Cart&#xf5;es de Cr&#xe9;dito" VSHIFT="29">
+<node CREATED="1579512394459" ID="ID_1049877028" MODIFIED="1579512415436" TEXT="Permite lan&#xe7;ar os cart&#xf5;es por planejamento"/>
+<node CREATED="1579512418192" ID="ID_1839045654" MODIFIED="1579512425708" TEXT="Pode ter mais de um cart&#xe3;o por planejamento"/>
+<node CREATED="1579512446816" ID="ID_1948447987" MODIFIED="1579512611344" TEXT="Informar data de melhor compra e data de pagamento"/>
+</node>
+<node CREATED="1579357543985" HGAP="40" ID="ID_641227625" MODIFIED="1579512625834" POSITION="left" TEXT="10 Movimentos de Cart&#xe3;o" VSHIFT="55">
+<node CREATED="1579512637265" ID="ID_28086741" MODIFIED="1579512650100" TEXT="Controle de acesso para lan&#xe7;amentos"/>
+<node CREATED="1579512661263" ID="ID_1285405791" MODIFIED="1579512673180" TEXT="Lan&#xe7;amentos retroativos at&#xe9; a data da &#xfa;ltima fatura"/>
+<node CREATED="1579512688600" ID="ID_1639657756" MODIFIED="1579512705916" TEXT="Usar os lan&#xe7;amentos de Grupos e SubGrupos"/>
+<node CREATED="1579512710959" ID="ID_166141830" MODIFIED="1579512715619" TEXT="Permitir lan&#xe7;amentos futuros"/>
+<node CREATED="1579512722631" ID="ID_1807189685" MODIFIED="1579512728340" TEXT="Digita&#xe7;&#xe3;o de parcelamentos"/>
+<node CREATED="1579512754535" ID="ID_850555372" MODIFIED="1579512775996" TEXT="Lan&#xe7;amentos controlados por data de pagamento do cart&#xe3;o"/>
+<node CREATED="1579512803384" ID="ID_396455573" MODIFIED="1579512821611" TEXT="Lan&#xe7;amentos de estorno de valores"/>
+</node>
+<node CREATED="1579356689887" HGAP="118" ID="ID_861976845" MODIFIED="1579389852901" POSITION="right" TEXT="6 - Previs&#xf5;es" VSHIFT="-98">
+<node CREATED="1579356728657" ID="ID_965996395" MODIFIED="1579356796512" TEXT="Definidos por Planejamento"/>
+<node CREATED="1579356800450" ID="ID_1692840538" MODIFIED="1579356832985" TEXT="Definidos por Per&#xed;odo (Dia / M&#xea;s / Ano)"/>
+<node CREATED="1579356838031" ID="ID_1654985341" MODIFIED="1579356850138" TEXT="Lan&#xe7;ados por Grupo e/ou SubGrupo"/>
+<node CREATED="1579356874683" ID="ID_1305588196" MODIFIED="1579357725693" TEXT="Tabela ZA5">
+<node CREATED="1579357683716" ID="ID_1703619587" MODIFIED="1579357704263" TEXT="Descri&#xe7;&#xe3;o: Lan&#xe7;amento de Previs&#xf5;es por Per&#xed;odo"/>
+</node>
+</node>
+<node CREATED="1579357784734" HGAP="30" ID="ID_1525460512" MODIFIED="1579512950228" POSITION="left" TEXT="11 - Consultas e Relat&#xf3;rios" VSHIFT="37">
+<node CREATED="1579512971456" ID="ID_1459956694" MODIFIED="1579513017070" TEXT="A ser definido">
+<icon BUILTIN="messagebox_warning"/>
+</node>
 </node>
 </node>
 </map>
